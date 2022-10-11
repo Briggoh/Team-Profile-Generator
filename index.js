74e5da9adb190ee74bcb8ea62ebcb4969f1024bc
Briggoh <H.e.briggs99@gmail.com>
2022-10-11 12:38:06 -0700

    pushing bones of near-finished product

--- index.js
+++ index.js
@@ -1,6 +1,119 @@
-// requiring inquire 
-const inquirer = require("inquirer");
-// requiring fs 
-const fs = require("fs");
-// requiring path 
-const path = require('path');
+// Requiring Inquirer package
+const inquirer = require('inquirer');
+// attaching employee file
+const Employee = require('./lib/Employee');
+// attaching manager file
+const Manager = require('./lib/Manager');
+// attaching engineer file 
+const Engineer = require('./lib/Engineer');
+// attaching intern file
+const Intern = require('./lib/Intern');
+// attaching generate page file
+const generatePage = require('./src/page-template');
+// attaching write file page
+const writeFile = require('./src/generate-site')
+
+let manager = [];
+let engineer = [];
+let intern = [];
+let employeeArr = {manager, engineer, intern};
+
+function Prompt() {
+    
+    return inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message:"What is the employee's role?",
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type:'text',
+            name: 'employee',
+            message: "What is the Employee's name?"
+        },
+        {
+            type:'text',
+            name: 'id',
+            message: "What is the employee's ID number?"
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "What is the employee's email?"
+        }])
+        .then(({employee, id, email, role}) => {
+            if (role === "Manager") {
+                return inquirer
+                    .prompt([{
+                        type:'text',
+                        name: 'office',
+                        message:"What is the Manager's office number?"
+                    },
+                    {
+                        type:'confirm',
+                        name:'anotherEntry',
+                        message: "What you like to add another employee?",
+                        default: false
+                    }])
+                    .then(({office, anotherEntry}) => {
+                        manager.push(new Manager(employee, id, email, office))
+                        // console.log(employeeArr)
+                        if (anotherEntry) {
+                            return Prompt();
+                        }
+                    })
+            } else if (role === "Engineer") {
+                return inquirer
+                    .prompt([{
+                        type: 'text',
+                        name: 'github',
+                        message: "What is the Engineer's Github username?"
+                    },
+                    {
+                        type:'confirm',
+                        name:'anotherEntry',
+                        message: "What you like to add another employee?",
+                        default: false
+                    }])
+                    .then(({github, anotherEntry}) => {
+                        engineer.push(new Engineer(employee, id, email, github))
+                        // console.log(employeeArr)
+                        if (anotherEntry) {
+                            return Prompt();
+                        }
+                    })
+            } else if (role === 'Intern') {
+                 return inquirer
+                    .prompt([{
+                        type:'text',
+                        name:'school',
+                        message: "What is the Intern's school?"
+                    },
+                    {
+                        type:'confirm',
+                        name:'anotherEntry',
+                        message: "What you like to add another employee?",
+                        default: false
+                    }])
+                    .then(({school, anotherEntry}) => {
+                        intern.push(new Intern(employee, id, email, school))
+                        // console.log(employeeArr)
+                        if (anotherEntry) {
+                            return Prompt();
+                        }
+                    })
+            }
+        })
+};
+
+
+
+Prompt()
+    .then(teamData => {
+        return generatePage(employeeArr)
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML)
+    })
